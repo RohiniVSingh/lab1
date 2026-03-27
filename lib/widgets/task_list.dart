e39d0e05b2656475c036cb748472fd518d508b13
srisahitichenna <srisahitichenna@gmail.com>
2026-03-27 11:19:44 +0100

    task

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/model/task_handler.dart';
+import 'package:provider/provider.dart';
 
 class TaskList extends StatelessWidget {
    const TaskList({super.key, required List<ListTile> children});
 
    @override
       Widget build(BuildContext context) {
-         return ListView(children: [
-            ListTile(title: Text('DAT216/TIG091')),
-            ListTile(title: Text('Lab1 Todos')),
-            ListTile(title: Text('En rad till')),
-         ]);
-      }
+
+        var taskHandler = context.watch<TaskHandler>();
+        var tasks = taskHandler.testTasks();
+
+   return ListView(children: [
+      for (final task in tasks) ListTile(title: Text(task.title)),
+   ]);
+ }
+    
 }
