68afb8e5846d196871f2635f17c9bd7043bd244d
Rohini Singh <rohiniv@chalmers.se>
2026-04-15 10:26:14 +0200

    changes

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/model/task_handler.dart';
+import 'package:lab1/model/types.dart';
 import 'package:lab1/widgets/task_list.dart';
+import 'package:lab1/views/add_view.dart';
+import 'package:provider/provider.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -10,14 +14,50 @@ class MainView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Things todo'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ), 
-        body: TaskList(
-          children: [
-            ListTile(title: Text('DAT216/TIG091')),
-            ListTile(title: Text('Lab1 Todos')),
-            ListTile(title: Text('En rad till')),
-        ],
+        actions: _actions(context),
       ),
-      );
+      body: TaskList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddView(),
+            ),
+          );
+        },
+        tooltip: 'Add todo',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  List<Widget> _actions(context) {
+    return [_filterMenu(context)];
+  }
+
+  Widget _filterMenu(context) {
+    var taskHandler = Provider.of<TaskHandler>(context, listen: false);
+
+    return PopupMenuButton<FilterType>(
+      initialValue: taskHandler.mode,
+      onSelected: (FilterType item) {
+        taskHandler.setMode(item);
+      },
+      itemBuilder: (BuildContext context) => [
+        const PopupMenuItem<FilterType>(
+          value: FilterType.all,
+          child: Text('All'),
+        ),
+        const PopupMenuItem<FilterType>(
+          value: FilterType.done,
+          child: Text('Done'),
+        ),
+        const PopupMenuItem<FilterType>(
+          value: FilterType.undone,
+          child: Text('Undone'),
+        ),
+      ],
+    );
   }
 }
--- lib/widgets/status_icon.dart
+++ lib/widgets/status_icon.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:lab1/model/task.dart';
+import 'package:lab1/model/task_handler.dart';
+import 'package:provider/provider.dart';
 
 class StatusIcon extends StatelessWidget {
   const StatusIcon(this.task, {super.key});
   final Task task;
 
   @override
-  Widget build(BuildContext context) {
-    final bool completed = task.completed;
+Widget build(BuildContext context) {
 
-    return Icon(completed ? Icons.check_circle : Icons.radio_button_unchecked,
-        color: completed ? Colors.green : null);
-  }
+   var taskHandler = context.watch<TaskHandler>();
+   var tasks = taskHandler.testTasks();
+
+return ListView(
+  children: [
+    for (final task in tasks)
+      ListTile(
+        leading: StatusIcon(task),
+        title: Text(task.title),
+        onTap: () {
+          taskHandler.toggleTask(task);
+        },
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () {
+            taskHandler.deleteTask(task);
+          },
+        ),
+      ),
+  ],
+);
+ }
 }
